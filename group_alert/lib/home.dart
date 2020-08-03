d62e8d418681ecff6141f723670650291205537f
Felix Waller <felixajwaller@gmail.com>
2020-08-03 10:48:43 +0100

    basic buttons and sizings (except people selector)

--- group_alert/lib/home.dart
+++ group_alert/lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:group_alert/appBar.dart' show MyAppBar;
 
 class HomeView extends StatefulWidget {
   @override
@@ -9,22 +10,63 @@ class HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text("Home")),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () {}, // TODO
-          )
-        ],
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Text("hello"),
-        ],
-      )
-    );
+        appBar: MyAppBar(),
+        body: Center(
+            child: IntrinsicWidth(
+                child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            // 3 favourite msgs
+            ButtonTheme(
+                minWidth: MediaQuery.of(context).size.width - 40,
+                child: FlatButton(
+                  child: Text('Lunch Time!'),
+                  onPressed: () {},
+                  color: Theme.of(context).backgroundColor,
+                )),
+            FlatButton(
+              child: Text('Supper Time!'),
+              onPressed: () {},
+              color: Theme.of(context).backgroundColor,
+            ),
+            FlatButton(
+              child: Text('Bedtime!'),
+              onPressed: () {},
+              color: Theme.of(context).backgroundColor,
+            ),
+
+            // send
+            Expanded(
+              child: RaisedButton(
+                child: Text("Send!"),
+                onPressed: () {},
+                color: Theme.of(context).accentColor,
+                shape: CircleBorder(),
+                elevation: 5,
+              ),
+            ),
+
+            // other msgs
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.chevron_left),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Text('Meeting!'),
+                onPressed: () {},
+                color: Theme.of(context).backgroundColor,
+              ),
+              IconButton(
+                icon: Icon(Icons.chevron_right),
+                onPressed: () {},
+              ),
+            ]),
+            Divider(),
+          ],
+        ))));
   }
 }
