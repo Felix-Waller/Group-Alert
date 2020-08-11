07a2e32f22476164f88a3cc5d925b9086e62bb5d
Felix Waller <felixajwaller@gmail.com>
2020-08-11 12:29:59 +0100

    added menu button navigation

--- group_alert/lib/appBar.dart
+++ group_alert/lib/appBar.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-class MyAppBar extends StatefulWidget implements PreferredSizeWidget{
+enum Menu { home, messages, settings }
+
+class MyAppBar extends StatefulWidget implements PreferredSizeWidget {
   MyAppBar({Key key}) : preferredSize = Size.fromHeight(kToolbarHeight), super(key: key);
 
   @override
@@ -14,34 +16,79 @@ class MyAppBarState extends State<MyAppBar> {
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      leading: IconButton(
+        icon: Icon(Icons.home),
+        onPressed: () => Navigator.of(context).pushReplacementNamed('/'),
+        ),
       title: Center(child: Text(getTitle())),
       actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {}, // TODO
-        )
+        PopupMenuButton(
+          onSelected: menuPressed,
+          itemBuilder: (BuildContext context) => <PopupMenuEntry<Menu>>[
+            const PopupMenuItem<Menu>(
+              value: Menu.home,
+              child: Text('Home'),
+            ),
+            const PopupMenuItem<Menu>(
+              value: Menu.messages,
+              child: Text('Messages'),
+            ),
+            const PopupMenuItem<Menu>(
+              value: Menu.settings,
+              child: Text('Settings'),
+            ),
+          ],
+        ),
       ],
     );
   }
 
-  String getTitle(){
-    switch(ModalRoute.of(context).settings.name){
-      case '/':{
-        return 'Home';
-      }
-      break;
-      case '/messages':{
-        return 'Messages';
-      }
-      break;
-      case '/settings':{
-        return 'Settings';
-      }
-      break;
-      default:{
-        return 'Group Alert';
-      }
-      break;
+  void menuPressed(Menu choice) {
+    switch (choice) {
+      case Menu.home:
+        {
+          Navigator.of(context).pushReplacementNamed('/');
+        }
+        break;
+      case Menu.messages:
+        {
+          Navigator.of(context).pushReplacementNamed('/messages');
+        }
+        break;
+      case Menu.settings:
+        {
+          Navigator.of(context).pushReplacementNamed('/settings');
+        }
+        break;
+      default:
+        {
+          return null;
+        }
+    }
+  }
+
+  String getTitle() {
+    switch (ModalRoute.of(context).settings.name) {
+      case '/':
+        {
+          return 'Home';
+        }
+        break;
+      case '/messages':
+        {
+          return 'Messages';
+        }
+        break;
+      case '/settings':
+        {
+          return 'Settings';
+        }
+        break;
+      default:
+        {
+          return 'Group Alert';
+        }
+        break;
     }
   }
 }
