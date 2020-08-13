285bdf8d2b3b79fe25994bcb01ae13d2edf7f138
Felix Waller <felixajwaller@gmail.com>
2020-08-13 11:42:04 +0100

    added text.dart

---
+++ group_alert/lib/widgets/text.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+InputDecoration textFieldInputDecoration(String hintText) {
+  return InputDecoration(
+      hintText: hintText,
+      hintStyle: TextStyle(color: Colors.black),
+      focusedBorder:
+          UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+      enabledBorder:
+          UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey)));
+}
+
+TextStyle simpleTextStyle() {
+  return TextStyle(color: Colors.black, fontSize: 16);
+}
+
+TextStyle biggerTextStyle() {
+  return TextStyle(color: Colors.black, fontSize: 17);
+}
