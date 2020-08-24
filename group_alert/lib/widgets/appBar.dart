c4772777d653dd1db12b1781403844b92e8a2933
Felix Waller <felixajwaller@gmail.com>
2020-08-24 15:01:08 +0100

    removed popup menu to seperate file

--- group_alert/lib/widgets/appBar.dart
+++ group_alert/lib/widgets/appBar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:group_alert/widgets/menuButton.dart';
 
 enum Menu { home, messages, settings }
 
@@ -22,49 +23,9 @@ class MyAppBarState extends State<MyAppBar> {
       ),
       title: Center(child: Text(getTitle())),
       actions: <Widget>[
-        PopupMenuButton(
-          onSelected: menuPressed,
-          itemBuilder: (BuildContext context) => <PopupMenuEntry<Menu>>[
-            const PopupMenuItem<Menu>(
-              value: Menu.home,
-              child: Text('Home'),
-            ),
-            const PopupMenuItem<Menu>(
-              value: Menu.messages,
-              child: Text('Messages'),
-            ),
-            const PopupMenuItem<Menu>(
-              value: Menu.settings,
-              child: Text('Settings'),
-            ),
-          ],
-        ),
+        MyMenuButton(),
       ],
     );
-  }
-
-  void menuPressed(Menu choice) {
-    switch (choice) {
-      case Menu.home:
-        {
-          Navigator.of(context).pushReplacementNamed('/');
-        }
-        break;
-      case Menu.messages:
-        {
-          Navigator.of(context).pushReplacementNamed('/messages');
-        }
-        break;
-      case Menu.settings:
-        {
-          Navigator.of(context).pushReplacementNamed('/settings');
-        }
-        break;
-      default:
-        {
-          return null;
-        }
-    }
   }
 
   String getTitle() {
---
+++ group_alert/lib/widgets/menuButton.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:group_alert/views/chatPage.dart';
+
+enum Menu { home, chat, messages, settings }
+
+class MyMenuButton extends StatefulWidget {
+  @override
+  MyMenuButtonState createState() => MyMenuButtonState();
+}
+
+class MyMenuButtonState extends State<MyMenuButton>{
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton(
+      onSelected: menuPressed,
+      itemBuilder: (BuildContext context) => <PopupMenuEntry<Menu>>[
+        const PopupMenuItem<Menu>(
+          value: Menu.home,
+          child: Text('Home'),
+        ),
+        const PopupMenuItem<Menu>(
+          value: Menu.chat,
+          child: Text('Chat')
+          ),
+        const PopupMenuItem<Menu>(
+          value: Menu.messages,
+          child: Text('Messages'),
+        ),
+        const PopupMenuItem<Menu>(
+          value: Menu.settings,
+          child: Text('Settings'),
+        ),
+      ],
+    );
+  }
+
+  menuPressed(Menu choice) {
+    switch (choice) {
+      case Menu.home:
+        {
+          Navigator.of(context).pushReplacementNamed('/');
+        }
+        break;
+      case Menu.chat:{
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (BuildContext context) => 
+            ChatPage(groupId: 'A8pyc3cw1LQG53NuliXb', userName: 'felix', groupName: 'groupTest1',)));
+      }
+      break;
+      case Menu.messages:
+        {
+          Navigator.of(context).pushReplacementNamed('/messages');
+        }
+        break;
+      case Menu.settings:
+        {
+          Navigator.of(context).pushReplacementNamed('/settings');
+        }
+        break;
+      default:
+        {
+          return null;
+        }
+    }
+  }
+}
