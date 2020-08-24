ae399ef1abde5565a96a4c5769a201a25566e903
Felix Waller <felixajwaller@gmail.com>
2020-08-24 16:12:14 +0100

    removed messages page

--- group_alert/lib/main.dart
+++ group_alert/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:group_alert/views/homePage.dart';
 import 'package:group_alert/views/loadingPage.dart';
-import 'package:group_alert/views/messagesPage.dart';
 import 'package:group_alert/views/settingsPage.dart';
 import 'package:group_alert/views/signInPage.dart';
 import 'package:group_alert/views/signUpPage.dart';
@@ -48,11 +47,9 @@ class MyApp extends StatelessWidget {
       initialRoute: userIsLoggedIn != null ? (userIsLoggedIn ? '/' : '/signIn') : '/signIn',
       routes: <String, WidgetBuilder>{
         '/': (BuildContext context) => new HomeView(),
-        '/messages': (BuildContext context) => new MessagesView(),
         '/settings': (BuildContext context) => new SettingsView(),
         '/signIn': (BuildContext context) => new SignInView(),
         '/signUp': (BuildContext context) => new SignUpView(),
-        // '/chat': (BuildContext context) => new ChatPage(), arguments dependent on runtime factors so remove from named routes?
       },
     );
   }
