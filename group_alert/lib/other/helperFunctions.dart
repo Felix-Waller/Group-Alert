611aeb839c08d1c1a18debdbe06bee1623204e6c
Felix Waller <felixajwaller@gmail.com>
2020-08-24 19:54:45 +0100

    add text sharedpreferences

--- group_alert/lib/other/helperFunctions.dart
+++ group_alert/lib/other/helperFunctions.dart
@@ -5,10 +5,11 @@ class HelperFunctions{
   static String sharedPreferenceUserLoggedInKey = "ISLOGGEDIN";
   static String sharedPreferenceUserNameKey = "USERNAMEKEY";
   static String sharedPreferenceUserEmailKey = "USEREMAILKEY";
+  
+  // text keys: TEXT1KEY, TEXT2KEY, TEXT3KEY
 
-  /// saving data to sharedpreference
+  // saving user data to sharedpreference
   static Future<bool> saveUserLoggedInSharedPreference(bool isUserLoggedIn) async{
-
     SharedPreferences preferences = await SharedPreferences.getInstance();
     return await preferences.setBool(sharedPreferenceUserLoggedInKey, isUserLoggedIn);
   }
@@ -23,7 +24,7 @@ class HelperFunctions{
     return await preferences.setString(sharedPreferenceUserEmailKey, userEmail);
   }
 
-  /// fetching data from sharedpreference
+  // fetching user data from sharedpreference
   static Future<bool> getUserLoggedInSharedPreference() async{
     SharedPreferences preferences = await SharedPreferences.getInstance();
     return preferences.getBool(sharedPreferenceUserLoggedInKey);
@@ -39,4 +40,15 @@ class HelperFunctions{
     return preferences.getString(sharedPreferenceUserEmailKey);
   }
 
+  // save text field
+  static Future<bool> saveTextSharedPreference(String text, String key) async{
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    return await preferences.setString(key, text);
+  }
+
+  // get text field
+  static Future<String> getTextSharedPreference(String key) async{
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    return preferences.getString(key) ?? "Default Message";
+  }
 }
