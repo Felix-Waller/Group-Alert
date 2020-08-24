c83bcb3bbc70b2aabb27d881f80ada61fc2567c7
Felix Waller <felixajwaller@gmail.com>
2020-08-24 19:57:11 +0100

    created msgEditTile

---
+++ group_alert/lib/widgets/msgEditTile.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:group_alert/other/helperFunctions.dart';
+
+class MsgEditTile extends StatefulWidget {
+  final String sharedPrefKey;
+
+  MsgEditTile(this.sharedPrefKey,);
+
+  @override
+  MsgEditTileState createState() => new MsgEditTileState();
+}
+
+class MsgEditTileState extends State<MsgEditTile> {
+  String text;
+  TextEditingController textEditController;
+
+  @override
+  initState() {
+    getText().then((value) => text = value ?? "Dinner Time!");
+    textEditController = new TextEditingController();
+    super.initState();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(text),
+      trailing: Icon(Icons.edit),
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+            title: Center(child: Text('Edit Text')),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  decoration: InputDecoration(hintText: "Edit Text"),
+                  controller: textEditController,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    FlatButton(
+                      child: Text('Cancel'),
+                      onPressed: () {
+                        textEditController.text = "";
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    FlatButton(
+                      child: Text('Done'),
+                      onPressed: () {
+                        setState(() {
+                          if (textEditController.text != null &&
+                              textEditController.text != "") {
+                            this.text = textEditController.text;
+                          }
+                        });
+                        textEditController.text = "";
+                        HelperFunctions.saveTextSharedPreference(this.text, widget.sharedPrefKey);
+                        Navigator.of(context).pop();
+                      },
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        );
+      },
+    );
+  }
+  
+  Future<String> getText() async {
+    return await HelperFunctions.getTextSharedPreference(widget.sharedPrefKey);
+  }
+}
