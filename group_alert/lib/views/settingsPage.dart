ca3e7d59398ca4eeb0d8903de590389ee498e5d6
Felix Waller <felixajwaller@gmail.com>
2020-08-24 19:57:05 +0100

    added msgEditTile added state to notifications selector added dialog to reset app

--- group_alert/lib/views/settingsPage.dart
+++ group_alert/lib/views/settingsPage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:group_alert/other/helperFunctions.dart';
 import 'package:group_alert/widgets/appBar.dart';
 import 'package:group_alert/services/auth.dart';
+import 'package:group_alert/widgets/msgEditTile.dart';
 
 class SettingsView extends StatefulWidget {
   @override
@@ -10,6 +11,8 @@ class SettingsView extends StatefulWidget {
 
 class SettingsViewState extends State<SettingsView> {
   String userName = '';
+  bool notificationsEnabled = true;
+
   @override
   Widget build(BuildContext context) {
     HelperFunctions.getUserNameSharedPreference().then((value) {
@@ -39,53 +42,43 @@ class SettingsViewState extends State<SettingsView> {
 
               // change password
               Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)),
-                child: ListTile(
-                  leading: Icon(Icons.lock),
-                  title: Text('Change Password'),
-                  trailing: Icon(Icons.chevron_right),
-                )
-              ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  child: ListTile(
+                    leading: Icon(Icons.lock),
+                    title: Text('Change Password'),
+                    trailing: Icon(Icons.chevron_right),
+                  )),
               // messages edit
               Card(
                 margin: EdgeInsets.fromLTRB(32, 8, 32, 8),
                 shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)),
+                    borderRadius: BorderRadius.circular(10)),
                 child: Column(
                   children: [
-                    ListTile(
-                      title: Text('Lunch Time!'),
-                      trailing: Icon(Icons.edit),
-                      onTap: () {},
-                    ),
+                    MsgEditTile('TEXT1KEY'),
                     _buildDivider(),
-                    ListTile(
-                      title: Text('Dinner Time!'),
-                      trailing: Icon(Icons.edit),
-                      onTap: () {},
-                    ),
+                    MsgEditTile('TEXT2KEY'),
                     _buildDivider(),
-                    ListTile(
-                      title: Text('Bedtime!'),
-                      trailing: Icon(Icons.edit),
-                      onTap: () {},
-                    ),
+                    MsgEditTile('TEXT3KEY'),
                   ],
                 ),
               ),
 
               // notifications enable
               Card(
-                margin: EdgeInsets.fromLTRB(32, 8, 32, 8),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
-                child: SwitchListTile(
-                  title: Text('Notifications'),
-                  value: true,
-                  onChanged: (val) {},
-                )
-              ),
+                  margin: EdgeInsets.fromLTRB(32, 8, 32, 8),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  child: SwitchListTile(
+                    title: Text('Notifications'),
+                    value: notificationsEnabled,
+                    onChanged: (val) {
+                      setState(() {
+                        notificationsEnabled = !notificationsEnabled;
+                      });
+                    },
+                  )),
 
               // danger zone
               Card(
@@ -94,77 +87,106 @@ class SettingsViewState extends State<SettingsView> {
                     borderRadius: BorderRadius.circular(10)),
                 child: Column(
                   children: <Widget>[
+                    // reset app
                     ListTile(
                       title: Center(
-                        child: Text('Reset', style: TextStyle(color: Colors.red))),
+                          child: Text('Reset',
+                              style: TextStyle(color: Colors.red))),
                       onTap: () {
-                        AuthService().signOut(); 
-                        // add other saved preferences as they are implemented
-                        Navigator.of(context).pushReplacementNamed('/signIn');
+                        showDialog(
+                            context: context,
+                            builder: (_) => AlertDialog(
+                                title: Center(child: Text('Reset App?')),
+                                content: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      FlatButton(
+                                        child: Text('No'),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                      FlatButton(
+                                        child: Text('Yes'),
+                                        onPressed: () {
+                                          AuthService().signOut();
+                                          HelperFunctions.saveTextSharedPreference("Default Message", "TEXT1KEY");
+                                          HelperFunctions.saveTextSharedPreference("Default Message", "TEXT2KEY");
+                                          HelperFunctions.saveTextSharedPreference("Default Message", "TEXT3KEY");
+                                          Navigator.of(context).pop();
+                                          Navigator.of(context).pushReplacementNamed('/signIn');
+                                        },
+                                      ),
+                                    ])));
                       },
                     ),
                     _buildDivider(),
+
+                    // delete account
                     ListTile(
                       title: Center(
-                        child: Text('Delete Account', style: TextStyle(color: Colors.red))),
+                          child: Text('Delete Account',
+                              style: TextStyle(color: Colors.red))),
                       onTap: () {
                         showDialog(
-                          context: context,
-                          builder: (_) => AlertDialog(
-                            title: Text('Delete Account?'),
-                            content:Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                FlatButton(
-                                  child: Text('No'),
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                ),
-                                FlatButton(
-                                  child: Text('Yes'),
-                                  onPressed: () {
-                                    AuthService().deleteAccount();
-                                    Navigator.of(context).pop();
-                                    Navigator.of(context).pushReplacementNamed('/signIn');
-                                  },
-                                ),
-                              ]
-                            )
-                          )
-                        );
+                            context: context,
+                            builder: (_) => AlertDialog(
+                                title: Center(child: Text('Delete Account?')),
+                                content: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      FlatButton(
+                                        child: Text('No'),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                      FlatButton(
+                                        child: Text('Yes'),
+                                        onPressed: () {
+                                          AuthService().deleteAccount();
+                                          Navigator.of(context).pop();
+                                          Navigator.of(context)
+                                              .pushReplacementNamed('/signIn');
+                                        },
+                                      ),
+                                    ])));
                       },
                     ),
                     _buildDivider(),
+
+                    // log out
                     ListTile(
                       title: Center(
-                        child: Text('Log Out', style: TextStyle(color: Colors.red))),
+                          child: Text('Log Out',
+                              style: TextStyle(color: Colors.red))),
                       onTap: () {
                         showDialog(
-                          context: context,
-                          builder: (_) => AlertDialog(
-                            title: Text('Log Out?'),
-                            content:Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                FlatButton(
-                                  child: Text('No'),
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                ),
-                                FlatButton(
-                                  child: Text('Yes'),
-                                  onPressed: () {
-                                    AuthService().signOut();
-                                    Navigator.of(context).pop();
-                                    Navigator.of(context).pushReplacementNamed('/signIn');
-                                  },
-                                ),
-                              ]
-                            )
-                          )
-                        );
+                            context: context,
+                            builder: (_) => AlertDialog(
+                                title: Center(child: Text('Log Out?')),
+                                content: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      FlatButton(
+                                        child: Text('No'),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                      FlatButton(
+                                        child: Text('Yes'),
+                                        onPressed: () {
+                                          AuthService().signOut();
+                                          Navigator.of(context).pop();
+                                          Navigator.of(context)
+                                              .pushReplacementNamed('/signIn');
+                                        },
+                                      ),
+                                    ])));
                       },
                     ),
                   ],
@@ -179,10 +201,9 @@ class SettingsViewState extends State<SettingsView> {
 
   Container _buildDivider() {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 10),
-      width: double.infinity,
-      height: 1.0,
-      color: Theme.of(context).backgroundColor
-    );
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        width: double.infinity,
+        height: 1.0,
+        color: Theme.of(context).backgroundColor);
   }
 }
